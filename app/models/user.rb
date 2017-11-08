67d527dd3cf0fc2ffed464c1595d64d308791488
Harsh shukla <hshukla1@mail.depaul.edu>
2017-11-08 00:41:56 +0000

    fin toy_app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-has_many :microposts
-  validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
-  validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
+has_many :microposts 
+  validates :show, presence: true    # Replace FILL_IN with the right code.
+  validates :edit, presence: true    # Replace FILL_IN with the right code.
+   validates:update, presence: true
+    validates :history, presence: true
+    validates :email, presence: true
+  validates :name, presence: true
 end
